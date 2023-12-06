234e1badc9282cea8b18d4613607f07226dffa04
Sam Lin <slin@masterlink.com.tw>
2023-12-06 11:25:44 +0800

    d6 scaffold

---
+++ 2023/day6/src/main.rs
@@ -0,0 +1,23 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("input")
+        // let contents = fs::read_to_string("example")
+        .expect("Should have been able to read the file");
+
+    println!("---------- part1 ----------");
+    // part 1
+    part1(&contents[..]);
+
+    println!("---------- part2 ----------");
+    // part 2
+    part2(&contents[..]);
+}
+
+fn part1(content: &str) {
+
+}
+
+fn part2(content: &str) {
+
+}
