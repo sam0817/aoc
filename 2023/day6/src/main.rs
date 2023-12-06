c9fb8e2744564367a9c4eb65543aef02ad491aba
Sam Lin <slin@masterlink.com.tw>
2023-12-06 13:26:05 +0800

    d6 ?_?

--- 2023/day6/src/main.rs
+++ 2023/day6/src/main.rs
@@ -14,10 +14,20 @@ fn main() {
     part2(&contents[..]);
 }
 
-fn part1(content: &str) {
-
+pub fn resolve(a: f64, b : f64, c: f64) -> (f64, f64) {
+    let item = b.powi(2) - 4f64 * a * c;
+    let x1 = (-b + item.sqrt()) / (2.0 * a);
+    let x2 = (-b - item.sqrt()) / (2.0 * a);
+    (x1, x2)
 }
+fn part1(content: &str) {}
 
 fn part2(content: &str) {
-
+    let a = 1_f64;
+    let b = -51926890_f64;
+    let c = 222203111261225_f64;
+    let (x1, x2) = resolve(a, b, c);
+    println!("x1: {}, x2: {}", x1, x2);
+    let num = x1 as isize - x2 as isize;
+    println!("num: {}", num)
 }
