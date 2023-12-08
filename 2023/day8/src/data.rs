3023ebeb8419fbc4386d670964b55dd424ece1b5
Sam Lin <slin@masterlink.com.tw>
2023-12-08 13:22:25 +0800

    day 8 part 1

---
+++ 2023/day8/src/data.rs
@@ -0,0 +1,5 @@
+struct Node {
+    label: String,
+    left: String,
+    right: String,
+}
--- 2023/day8/src/main.rs
+++ 2023/day8/src/main.rs
@@ -1,8 +1,11 @@
+use std::collections::HashMap;
 use std::fs;
+
+pub mod data;
 
 fn main() {
     let contents = fs::read_to_string("input")
-        // let contents = fs::read_to_string("example")
+    // let contents = fs::read_to_string("example")
         .expect("Should have been able to read the file");
 
     println!("---------- part1 ----------");
@@ -14,7 +17,51 @@ fn main() {
     part2(&contents[..]);
 }
 
-fn part1(content: &str) {}
+fn part1(content: &str) {
+    let mut line_no = 0;
+    let mut instructions = Vec::<char>::new();
+    let mut nodes = HashMap::<String, (String, String)>::new();
+    content.lines().for_each(|line| {
+        line_no += 1;
+        if line_no == 1 {
+            line.chars().for_each(|c| { instructions.push(c); });
+            return;
+        }
+        if line_no == 2 { return; }
+        let a = line[0..3].to_string();
+        let b = line[7..10].to_string();
+        let c = line[12..15].to_string();
+        nodes.insert(a, (b, c));
+        // println!("{}: {}", line_no, line);
+        // println!("{}: {} {} {}", line_no, a, b, c);
+        // println!("{}: {:?}", line_no, nodes);
+    });
 
-fn part2(content: &str) {
+    println!("instructions: {:?}", instructions);
+    let mut step = 0;
+    let mut position = String::from("AAA");
+    'label: loop {
+        instructions.iter().for_each(|c| {
+            let node = nodes.get(&position).unwrap();
+            match c {
+                'L' => {
+                    step += 1;
+                    println!("{}: {} -> {}", step, position, node.0);
+                    position = node.0.to_string();
+                },
+                'R' =>{
+                    step += 1;
+                    println!("{}: {} -> {}", step, position, node.1);
+                    position = node.1.to_string();
+                },
+                _ => {},
+            }
+            if position == "ZZZ" { return; }
+        });
+        if position == "ZZZ" { break 'label; }
+    }
+    println!("step: {}", step);
 }
+
+fn part2(content: &str) {}
+
