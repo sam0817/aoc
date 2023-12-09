c4e2c53b21a90eed7ad1a703dd472e8415308888
Sam Lin <sam0817@gmail.com>
2023-12-09 13:29:57 +0800

    day 9

--- 2023/day9/src/main.rs
+++ 2023/day9/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, hash::Hash, collections::HashMap};
 
 fn main() {
     let contents = fs::read_to_string("input")
@@ -14,8 +14,73 @@ fn main() {
     part2(&contents[..]);
 }
 
-fn part1(content: &str) {}
+
+fn part1(content: &str) {
+    let mut sum = 0;
+    content.lines().for_each(|line| {
+        let nums =line.split(" ").collect::<Vec<&str>>()
+            .iter()
+            .map(|x| x.parse::<isize>().unwrap())
+            .collect::<Vec<isize>>();
+        let n = find_next(&nums);
+        sum += n;
+    });
+    println!("sum: {}", sum);
+}
+
+fn find_next(nums: &Vec<isize>) -> isize {
+    let mut step = 0_isize;
+    let mut map = HashMap::<isize, Vec<isize>>::new();
+    map.insert(0, nums.to_vec());
+    loop {
+        let origin = map.get(&step).unwrap();
+        step += 1;
+        let mut next = Vec::<isize>::new();
+        for i in 0..(origin.len()-1) {
+            next.push(origin[i+1] - origin[i]);
+        }
+        map.insert(step, next.to_vec());
+        if next.iter().all(|x| x == &0) { break;}
+    }
+    let mut sum = 0;
+    for i in 0..(step+1) {
+        let list =  map.get(&i).unwrap();
+        sum += list.last().unwrap();
+    }
+    sum
+}
 
 fn part2(content: &str) {
-    
+    let mut sum = 0;
+    content.lines().for_each(|line| {
+        let nums =line.split(" ").collect::<Vec<&str>>()
+            .iter()
+            .map(|x| x.parse::<isize>().unwrap())
+            .collect::<Vec<isize>>();
+        let n = find_next2(&nums);
+        sum += n;
+    });
+    println!("sum: {}", sum);
+}
+
+fn find_next2(nums: &Vec<isize>) -> isize {
+    let mut step = 0_isize;
+    let mut map = HashMap::<isize, Vec<isize>>::new();
+    map.insert(0, nums.to_vec());
+    loop {
+        let origin = map.get(&step).unwrap();
+        step += 1;
+        let mut next = Vec::<isize>::new();
+        for i in 0..(origin.len()-1) {
+            next.push(origin[i+1] - origin[i]);
+        }
+        map.insert(step, next.to_vec());
+        if next.iter().all(|x| x == &0) { break;}
+    }
+    let mut sum = 0; // nums.first().unwrap().clone();
+    for i in 0..(step+1) {
+        let list =  map.get(&i).unwrap();
+        sum += list.first().unwrap() * (-1_isize).pow(i as u32);
+    }
+    sum
 }
