8368600ca6aae3b9af5d0b075e01c8d5cc28ce4f
Sam Lin <slin@masterlink.com.tw>
2023-12-19 13:36:14 +0800

    day 19 parse flows

--- 2023/day19/src/main.rs
+++ 2023/day19/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 use std::fs;
 
 fn main() {
@@ -16,9 +16,9 @@ fn main() {
 }
 
 
-fn parse_data(content: &str) -> (HashMap<String, String>, Vec<HashMap<PartType,usize>>) {
-    let mut workflows = HashMap::<String, String>::new();
-    let mut parts = Vec::<HashMap<PartType,usize>>::new();
+fn parse_data(content: &str) -> (BTreeMap<String, WorkFlow>, Vec<HashMap<PartType, usize>>) {
+    let mut workflows = BTreeMap::<String, WorkFlow>::new();
+    let mut parts = Vec::<HashMap<PartType, usize>>::new();
     let mut flow_finished = false;
     content.lines().for_each(|line| {
         if (line.len() == 0) {
@@ -29,19 +29,58 @@ fn parse_data(content: &str) -> (HashMap<String, String>, Vec<HashMap<PartType,u
             let part = parse_parts(line);
             parts.push(part);
         } else {
-            let workflow = line.split("{").collect::<Vec<&str>>();
-            workflows.insert(workflow[0].to_string(), workflow[1].to_string());
+            let (name, flow) = parse_flow(line);
+            workflows.insert(name, flow);
         }
     });
     (workflows, parts)
 }
 
-fn parse_flow(str: &str) -> Vec<String> {
-    let mut result = Vec::<String>::new();
-    // str.split("->").for_each(|s| {
-    //     result.push(s.trim().to_string());
-    // });
-    result
+fn parse_flow(data: &str) -> (String, WorkFlow) {
+    let split = data.split("{").collect::<Vec<&str>>();
+    let key = split[0].trim().to_string();
+    let mut split_value = split[1][0..(split[1].len() - 1)].split(",").collect::<Vec<&str>>();
+    let mut criteria = Vec::<(PartType, CompareType, usize, String)>::new();
+    let else_next = split_value[split_value.len() - 1].to_string();
+    split_value.remove(split_value.len() - 1);
+    split_value.iter().for_each(|s| {
+        let split = s.split(":").collect::<Vec<&str>>();
+        let condition = split[0].trim();
+        let dest = split[1].trim().to_string();
+        let gt = &condition.split(">").collect::<Vec<&str>>();
+        if gt.len() > 1 {
+            let part_type = match gt[0].trim() {
+                "x" => PartType::X,
+                "m" => PartType::M,
+                "a" => PartType::A,
+                "s" => PartType::S,
+                _ => PartType::X,
+            };
+            let compare_value = gt[1].trim().parse::<usize>().unwrap();
+            criteria.push((part_type, CompareType::Greater, compare_value, dest));
+            return;
+        }
+        let lt = &condition.split("<").collect::<Vec<&str>>();
+        if lt.len() > 1 {
+            let part_type = match lt[0].trim() {
+                "x" => PartType::X,
+                "m" => PartType::M,
+                "a" => PartType::A,
+                "s" => PartType::S,
+                _ => PartType::X,
+            };
+            let compare_value = lt[1].trim().parse::<usize>().unwrap();
+            criteria.push((part_type, CompareType::Less, compare_value, dest));
+            return;
+        }
+    });
+    (key, WorkFlow { criteria, else_next })
+}
+
+#[derive(Debug, Clone)]
+struct WorkFlow {
+    criteria: Vec<(PartType, CompareType, usize, String)>,
+    else_next: String,
 }
 
 fn parse_parts(data: &str) -> HashMap<PartType, usize> {
@@ -68,6 +107,13 @@ fn parse_parts(data: &str) -> HashMap<PartType, usize> {
         }
     });
     result
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum CompareType {
+    Less,
+    Greater,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
