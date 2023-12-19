303607b9636cccef1b9f4e9cfabe8c5016c7d0d7
Sam Lin <slin@masterlink.com.tw>
2023-12-19 13:17:50 +0800

    day 19 parse parts

--- 2023/day19/src/main.rs
+++ 2023/day19/src/main.rs
@@ -16,9 +16,9 @@ fn main() {
 }
 
 
-fn parse_data(content: &str) -> (HashMap<String, String>, Vec<String>) {
+fn parse_data(content: &str) -> (HashMap<String, String>, Vec<HashMap<PartType,usize>>) {
     let mut workflows = HashMap::<String, String>::new();
-    let mut parts = Vec::<String>::new();
+    let mut parts = Vec::<HashMap<PartType,usize>>::new();
     let mut flow_finished = false;
     content.lines().for_each(|line| {
         if (line.len() == 0) {
@@ -26,13 +26,56 @@ fn parse_data(content: &str) -> (HashMap<String, String>, Vec<String>) {
             return;
         }
         if flow_finished {
-            parts.push(line.to_string());
+            let part = parse_parts(line);
+            parts.push(part);
         } else {
             let workflow = line.split("{").collect::<Vec<&str>>();
             workflows.insert(workflow[0].to_string(), workflow[1].to_string());
         }
     });
     (workflows, parts)
+}
+
+fn parse_flow(str: &str) -> Vec<String> {
+    let mut result = Vec::<String>::new();
+    // str.split("->").for_each(|s| {
+    //     result.push(s.trim().to_string());
+    // });
+    result
+}
+
+fn parse_parts(data: &str) -> HashMap<PartType, usize> {
+    let mut result = HashMap::<PartType, usize>::new();
+    let line = data[1..(data.len()) - 1].split(",").collect::<Vec<&str>>();
+    line.iter().for_each(|s| {
+        let split = s.split("=").collect::<Vec<&str>>();
+        let key = split[0].trim();
+        let value = split[1].trim().parse::<usize>().unwrap();
+        match key {
+            "x" => {
+                result.insert(PartType::X, value);
+            }
+            "m" => {
+                result.insert(PartType::M, value);
+            }
+            "a" => {
+                result.insert(PartType::A, value);
+            }
+            "s" => {
+                result.insert(PartType::S, value);
+            }
+            _ => {}
+        }
+    });
+    result
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum PartType {
+    X,
+    M,
+    A,
+    S,
 }
 
 fn part1(content: &str) {
