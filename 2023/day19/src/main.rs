0a9fb4f8daf47072e3bd46d93048a5c5ea072f86
Sam Lin <slin@masterlink.com.tw>
2023-12-19 13:10:45 +0800

    day 19 parse

--- 2023/day19/src/main.rs
+++ 2023/day19/src/main.rs
@@ -1,8 +1,9 @@
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
     // let contents = fs::read_to_string("input")
-        let contents = fs::read_to_string("example")
+    let contents = fs::read_to_string("example")
         .expect("Should have been able to read the file");
 
     println!("---------- part1 ----------");
@@ -15,10 +16,29 @@ fn main() {
 }
 
 
+fn parse_data(content: &str) -> (HashMap<String, String>, Vec<String>) {
+    let mut workflows = HashMap::<String, String>::new();
+    let mut parts = Vec::<String>::new();
+    let mut flow_finished = false;
+    content.lines().for_each(|line| {
+        if (line.len() == 0) {
+            flow_finished = true;
+            return;
+        }
+        if flow_finished {
+            parts.push(line.to_string());
+        } else {
+            let workflow = line.split("{").collect::<Vec<&str>>();
+            workflows.insert(workflow[0].to_string(), workflow[1].to_string());
+        }
+    });
+    (workflows, parts)
+}
+
 fn part1(content: &str) {
-
+    let (map, parts) = parse_data(content);
+    println!("{:?}", map);
+    println!("{:?}", parts);
 }
 
-fn part2(content: &str) {
-
-}
+fn part2(content: &str) {}
