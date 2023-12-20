ddaf2e942e62fdb5e493e57efe81e99c5103788f
Sam Lin <slin@masterlink.com.tw>
2023-12-20 13:32:20 +0800

    day 20 parse

--- 2023/day20/src/main.rs
+++ 2023/day20/src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 fn main() {
     // let contents = fs::read_to_string("input")
-        let contents = fs::read_to_string("example")
+    let contents = fs::read_to_string("example")
         .expect("Should have been able to read the file");
 
     println!("---------- part1 ----------");
@@ -14,16 +14,61 @@ fn main() {
     part2(&contents[..]);
 }
 
-fn parse_data(content: &str) {
+#[derive(Debug, Copy, Clone)]
+enum NodeType {
+    FlipFlop,
+    Conjunction,
+    Broadcast,
+}
+
+#[derive(Debug, Clone)]
+struct Node {
+    name: String,
+    node_type: NodeType,
+    receivers: Vec<String>,
+}
+
+fn parse_data(content: &str) ->Vec<Node> {
+    let mut nodes = vec![];
     content.lines().for_each(|line| {
-       println!("{}", line)
+        let s = line.split(" -> ").collect::<Vec<&str>>();
+        let sender = s[0];
+        let mut node = match sender.chars().nth(0).unwrap() {
+            '%' => {
+                let name = sender[1..].to_string();
+                Node {
+                    name,
+                    node_type: NodeType::FlipFlop,
+                    receivers: vec![],
+                }
+            }
+            '&' => {
+                let name = sender[1..].to_string();
+                Node {
+                    name,
+                    node_type: NodeType::Conjunction,
+                    receivers: vec![],
+                }
+            }
+            _ => {
+                Node {
+                    name: sender.to_string(),
+                    node_type: NodeType::Broadcast,
+                    receivers: vec![],
+                }
+            }
+        };
+        let receivers = s[1].split(", ")
+            .map(|r| r.trim().to_string())
+            .collect::<Vec<String>>();
+        node.receivers = receivers;
+        nodes.push(node);
     });
+    nodes
 }
 
 fn part1(content: &str) {
     parse_data(content);
 }
 
-fn part2(content: &str) {
-
-}
+fn part2(content: &str) {}
